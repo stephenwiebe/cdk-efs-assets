f8ce18d31b7aeff3ffb096ea59dd4f102a535a15
Pahud <pahudnet@gmail.com>
2020-12-19 22:11:29 +0800

    upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,5 @@
 const {
-  AwsCdkConstructLibrary,
-  GithubWorkflow
+  AwsCdkConstructLibrary
 } = require('projen');
 
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
@@ -8,7 +7,7 @@ const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 const project = new AwsCdkConstructLibrary({
   authorAddress: "pahudnet@gmail.com",
   authorName: "Pahud Hsieh",
-  cdkVersion: "1.67.0",
+  cdkVersion: "1.77.0",
   name: "cdk-efs-assets",
   repository: "https://github.com/pahud/cdk-efs-assets.git",
   description: 'Amazon EFS assets from Github repositories or S3 buckets',
@@ -42,12 +41,12 @@ const project = new AwsCdkConstructLibrary({
 });
 
 // create a custom projen and yarn upgrade workflow
-const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+const workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
 workflow.on({
   schedule: [{
-    cron: '0 6 * * *'
-  }], // 6am every day
+    cron: '11 0 * * *'
+  }], // 0:11am every day
   workflow_dispatch: {}, // allow manual triggering
 });
 
@@ -55,24 +54,21 @@ workflow.addJobs({
   upgrade: {
     'runs-on': 'ubuntu-latest',
     'steps': [
-      ...project.workflowBootstrapSteps,
-
-      // yarn upgrade
-      {
-        run: `yarn upgrade`
+      { uses: 'actions/checkout@v2' },
+      { 
+        uses: 'actions/setup-node@v1',
+        with: {
+          'node-version': '10.17.0',
+        }
       },
-
-      // upgrade projen
-      {
-        run: `yarn projen:upgrade`
-      },
-
+      { run: `yarn upgrade` },
+      { run: `yarn projen:upgrade` },
       // submit a PR
       {
         name: 'Create Pull Request',
         uses: 'peter-evans/create-pull-request@v3',
         with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + '}}',
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
           'commit-message': 'chore: upgrade projen',
           'branch': 'auto/projen-upgrade',
           'title': 'chore: upgrade projen and yarn',
@@ -83,7 +79,6 @@ workflow.addJobs({
     ],
   },
 });
-
 
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
